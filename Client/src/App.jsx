aac06f32f00644ad9354aa132f889b317a38b376
Sulav Gautam <118454666+SulAv100@users.noreply.github.com>
2024-09-19 17:12:58 +0545

    feat: sidebar, active denote

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import SideBar from "./components/SideBar/SideBar";
+import NavBar from "./components/NavBar/NavBar";
+import './App.css'
 
 export default function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<NavBar />} />
-      </Routes>
+      <div className="navigation-elem">
+        <SideBar />
+        <NavBar />
+      </div>
+      <Routes></Routes>
     </>
   );
 }
--- Client/src/components/SideBar/SideBar.jsx
+++ Client/src/components/SideBar/SideBar.jsx
@@ -1,8 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SideBar.css";
+import Logo from "../../assets/logo.svg";
 
 function SideBar() {
-  return <div>SideBar</div>;
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const handleActive =(index)=>{
+    setActiveIndex(index);
+  }
+
+  return (
+    <aside className="sidebar">
+      <div className="logo-cont">
+        <img src={Logo} alt="logo" />
+      </div>
+      <div className="page-navigator">
+        <ul>
+          <li
+            className={activeIndex === 0 ? "active" : ""}
+            onClick={() => handleActive(0)}
+          >
+            <span>
+              <i className="fa-solid fa-house"></i>
+              <p>Dashboard</p>
+            </span>
+          </li>
+          <li
+            className={activeIndex === 1 ? "active" : ""}
+            onClick={() => handleActive(1)}
+          >
+            <span>
+              <i className="fa-solid fa-dollar-sign"></i>
+              <p>Sales</p>
+            </span>
+          </li>
+          <li
+            className={activeIndex === 2 ? "active" : ""}
+            onClick={() => handleActive(2)}
+          >
+            <span>
+              <i className="fa-solid fa-store"></i>
+              <p>Inventory</p>
+            </span>
+            <i className="fa-solid fa-greater-than"></i>
+          </li>
+          <li
+            className={activeIndex === 3 ? "active" : ""}
+            onClick={() => handleActive(3)}
+          >
+            <span>
+              <i className="fa-solid fa-tag"></i>
+              <p>Purchase</p>
+            </span>
+          </li>
+          <li
+            className={activeIndex === 4 ? "active" : ""}
+            onClick={() => handleActive(4)}
+          >
+            <span>
+              <i className="fa-solid fa-file"></i>
+              <p>Reports</p>
+            </span>
+          </li>
+        </ul>
+      </div>
+    </aside>
+  );
 }
 
 export default SideBar;
