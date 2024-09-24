46e3c1077df15d48a0e657f522446b6afe8d5629
Beasty002 <xasbin12@gmail.com>
2024-09-24 14:48:20 +0545

    feat : add brand css

--- Client/src/popups/AddBrand/AddBrand.jsx
+++ Client/src/popups/AddBrand/AddBrand.jsx
@@ -4,33 +4,33 @@ import './AddBrand.css'
 export default function AddBrand() {
     return (
         <>
-            .overlay
+            <div className="overlay"></div>
             <section id='addBrand'>
                 <h2>New Brand</h2>
                 <form action="">
                     <div className="form-container">
-                        <label htmlFor="">Brand Title</label>
+                        <label HTMLfor="">Brand Title</label>
                         <input type="text" placeholder='eg: mayur, creata .....' name="" id="" />
                     </div>
                     <div className="form-container">
-                        <p>Does this brands'product have multile variations ?</p>
+                        <label>Does this brands'product have multile variations ?</label>
                         <div className="radio-form-container">
                             <div className="radio-form">
-                                <input type="radio" name="variation" id="variationYes" />
-                                <label htmlFor="varationYes">Yes</label>
+                                <input type="radio" name="variation" id="variationYes" checked />
+                                <label HTMLfor="variationYes">Yes</label>
                             </div>
                             <div className="radio-form">
                                 <input type="radio" name="variation" id="variationNo" />
-                                <label htmlFor="varationNo">Yes</label>
+                                <label HTMLfor="variationNo">No</label>
                             </div>
                         </div>
                     </div>
                     <div className="form-container">
-                        <label htmlFor="">Row Label</label>
+                        <label HTMLfor="">Row Label</label>
                         <input type="text" placeholder='eg size , code ....' />
                     </div>
                     <div className="form-container">
-                        <label htmlFor="">Column Label</label>
+                        <label HTMLfor="">Column Label</label>
                         <input type="text" placeholder='eg name , mm ....' />
                     </div>
                     <div className="btn-container">
@@ -38,7 +38,8 @@ export default function AddBrand() {
                         <button type="submit" className='primary-btn'>Add</button>
                     </div>
                 </form>
-            </section></>
+            </section>
+        </>
 
     )
 }
