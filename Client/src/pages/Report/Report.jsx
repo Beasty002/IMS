df21d01fb6fa26270b634f22c91d63e72a67f818
Beasty002 <xasbin12@gmail.com>
2024-09-25 17:47:58 +0545

    feat :  brand item list page

--- Client/src/pages/Report/Report.jsx
+++ Client/src/pages/Report/Report.jsx
@@ -25,7 +25,43 @@ function Report() {
             </button>
           </div>
         </section>
+      </section>
 
+      <section>
+        <div className="brand-item-table">
+          <table>
+            <thead>
+              <tr>
+                <th className='table-checkbox'><input type="checkbox" /></th>
+                <th>Size/mm</th>
+                <th>6mm</th>
+                <th>10mm</th>
+                <th>16mm</th>
+                <th className='table-action-container'>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+
+              <tr>
+                <td className='table-checkbox'>
+                  <input type="checkbox" />
+                </td>
+                <td>1000</td>
+                <td>1000</td>
+                <td>12</td>
+                <td>12</td>
+                <td className='table-action-container'>
+                  <div className="action-container">
+                    <i className="bx bx-edit-alt edit-icon"></i>
+                    <i className="bx bx-trash del-icon"></i>
+                  </div>
+
+                </td>
+              </tr>
+            </tbody>
+          </table>
+
+        </div>
       </section>
 
 
