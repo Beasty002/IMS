8d1e7172fa9edbebc65f4d9603e366e67fc353a9
Beasty002 <xasbin12@gmail.com>
2024-09-26 11:03:01 +0545

    feat : report print popup

--- Client/src/pages/Report/Report.jsx
+++ Client/src/pages/Report/Report.jsx
@@ -3,9 +3,36 @@ import './Report.css'
 
 function Report() {
   return (
-    <>
-      report
-    </>
+
+    <section>
+      <h1>Categories</h1>
+      <div className="cat-list-top">
+        <div className="search-box">
+          <i className='bx bx-search-alt'></i>
+          <input
+            type="text"
+            placeholder="Search categories..."
+            aria-label="Search input"
+          />
+        </div>
+        <div className="btn-container">
+          <button className="primary-btn"><i class='bx bxs-printer'></i> Print</button>
+        </div>
+
+
+      </div>
+      <div className="cat-list">
+        <div className="cat-box">
+          <h3>Plywood</h3>
+          <p className="cat-stock-availability">Stock available : 80</p>
+          <div className="action-container">
+            <i class='bx bx-printer print-icon' ></i>
+
+          </div>
+        </div>
+      </div>
+    </section>
+
   )
 }
 
---
+++ Client/src/popups/ReportPrint/ReportPrint.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export default function ReportPrint() {
+    return (
+        <section class="print-popup">
+            <h1>Print Categories</h1>
+            <section class="cat-list-to-print">
+                <div class="cat-print-container">
+                    <input type="checkbox" name="" id="selAll" /> <label htmlFor="selAll">Select All</label>
+                </div>
+                <div class="cat-print-container">
+                    <input type="checkbox" name="" id="" /> <label htmlFor="">Plywood</label>
+                </div>
+                <div class="cat-print-container">
+                    <input type="checkbox" name="" id="" /> <label htmlFor="">Doors</label>
+                </div>
+                <div class="cat-print-container">
+                    <input type="checkbox" name="" id="" /> <label htmlFor="">laminates</label>
+                </div>
+            </section>
+            <div class="btn-container">
+                <button class="cancel-btn">Cancel</button>
+                <button class="primary-btn">Print</button>
+            </div>
+        </section>
+
+    )
+}
