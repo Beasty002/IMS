8ee3baa10bc2959d944699615538d15bf9066927
Sulav Gautam <118454666+SulAv100@users.noreply.github.com>
2024-09-26 17:31:44 +0545

    feat: Retrieve respective category brand in inventory

--- Server/controllers/data-controller.js
+++ Server/controllers/data-controller.js
@@ -1,212 +1,219 @@
-const path = require("path")
-const Category = require("../models/category-model")
-const Brand = require("../models/brand-model")
-const { Collection } = require("mongoose")
-
+const path = require("path");
+const Category = require("../models/category-model");
+const Brand = require("../models/brand-model");
+const { Collection } = require("mongoose");
 
 ///////////////////////////// CATEGORIES //////////////////////////////
-const getAllCategories = async (req,res) => {
-    try{
-        const categories = await Category.find()
+const getAllCategories = async (req, res) => {
+  try {
+    const categories = await Category.find();
 
-        if (!categories || categories.length == 0)
-        {
-            return res.status(404).json({err: "No categories found!"});
-        }
-
-        return res.json({ cats: categories});
+    if (!categories || categories.length == 0) {
+      return res.status(404).json({ err: "No categories found!" });
     }
-    catch(err){
-        console.log("error in fetching categories!")
+
+    return res.json({ cats: categories });
+  } catch (err) {
+    console.log("error in fetching categories!");
+  }
+};
+
+const createCategory = async (req, res) => {
+  try {
+    const cat = req.body.category;
+
+    console.log(cat);
+
+    const newCat = new Category({
+      title: cat,
+    });
+
+    await newCat.save();
+
+    res.json({ msg: cat + " category created!" });
+  } catch (err) {
+    console.error("Error creating category:", err);
+    res.status(500).json({ message: "Internal server error!!(createCat)" });
+  }
+};
+
+const delCategory = async (req, res) => {
+  try {
+    const delCat = req.body.delCat;
+
+    if (!delCat) {
+      console.log("No such category found!");
+      return res.json({ err: "No such category found!" });
     }
-}
 
-const createCategory = async (req,res) => {
-    try{
-        const cat = req.body.category
+    await Category.deleteOne({ title: delCat });
 
-        console.log(cat)
+    return res.json({ msg: delCat + " category deleted!" });
+  } catch (err) {
+    console.error("Error deleting category:", err);
+    res.status(500).json({ message: "Internal server error!!(delCat)" });
+  }
+};
 
-        const newCat = new Category({
-            title: cat
-        });
+const editCategory = async (req, res) => {
+  try {
+    const editCatId = req.body.editCatId;
+    const editCatName = req.body.editCatName;
 
-        await newCat.save();
+    const toUpdateCat = await Category.findById({ _id: editCatId });
 
-        res.json({msg: cat + " category created!"})
-
+    if (!toUpdateCat) {
+      return res.json({ err: "No such category found!" });
     }
-    catch(err){
-        console.error("Error creating category:", err);
-        res.status(500).json({ message: "Internal server error!!(createCat)" });
-    }
-}
 
-const delCategory = async(req,res) =>{
-    try{
-        const delCat = req.body.delCat
+    toUpdateCat.title = editCatName;
 
-        if (!delCat){
-            console.log("No such category found!")
-            return res.json({ err: "No such category found!"});
-        }
+    toUpdateCat.save();
 
-        await Category.deleteOne({ title: delCat});
-
-        return res.json({msg: delCat+" category deleted!"});
-
-    }
-    catch(err){
-        console.error("Error deleting category:", err);
-        res.status(500).json({ message: "Internal server error!!(delCat)" });
-    }
-}
-
-const editCategory = async(req,res) => {
-    try{
-        const editCatId = req.body.editCatId
-        const editCatName = req.body.editCatName
-
-        const toUpdateCat = await Category.findById({ _id: editCatId})
-
-        if(!toUpdateCat){
-            return res.json({ err: "No such category found!"})
-        }
-
-        toUpdateCat.title = editCatName
-
-        toUpdateCat.save()
-
-        return res.json({ updated: toUpdateCat})
-    }
-    catch(err){
-        console.error("Error editing category:");
-        res.status(500).json({ message: "Internal server error!!(editCat)" });
-    }
-}
-
+    return res.json({ updated: toUpdateCat });
+  } catch (err) {
+    console.error("Error editing category:");
+    res.status(500).json({ message: "Internal server error!!(editCat)" });
+  }
+};
 
 ////////////////////////////////////// BRANDS ///////////////////////////////////////////////////
+// no need for this one
+// const getAllBrands = async (req, res) => {
+//   try {
+//     const brands = await Brand.find();
 
-const getAllBrands = async (req,res) => {
-    try{
-        const brands = await Brand.find()
+//     if (!brands || brands.length == 0) {
+//       return res.status(404).json({ err: "No brands found!" });
+//     }
 
-        if (!brands || brands.length == 0)
-        {
-            return res.status(404).json({err: "No brands found!"});
-        }
+//     return res.json({ brands: brands });
+//   } catch (err) {
+//     console.log("error in fetching brands!");
+//   }
+// };
 
-        return res.json({ brands: brands});
+const getSpecificBrand = async (req, res) => {
+  try {
+    const { categoryName } = req.body;
+    const requiredBrand = await Brand.find({ parentCategory: categoryName });
+
+    if (requiredBrand.length === 0) {
+      return res
+        .status(400)
+        .json({ msg: "No data related to given brand found" });
     }
-    catch(err){
-        console.log("error in fetching brands!")
+
+    return res.status(202).json({ brands: requiredBrand });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ msg: "Server error" });
+  }
+};
+
+const createBrand = async (req, res) => {
+  try {
+    let colLabel;
+
+    const brandName = req.body.brandName;
+    const multiVar = req.body.multiVar;
+    const rowLabel = req.body.rowLabel;
+
+    if (req.body.colLabel) colLabel = req.body.colLabel;
+
+    const parentCat = req.body.parentCat;
+
+    let newBrand;
+
+    if (multiVar === "yes") {
+      newBrand = new Brand({
+        brandName: brandName,
+        multiVar: multiVar,
+        rowLabel: rowLabel,
+        colLabel: colLabel,
+        parentCategory: parentCat,
+      });
+    } else if (multiVar === "no") {
+      newBrand = new Brand({
+        brandName: brandName,
+        multiVar: multiVar,
+        rowLabel: rowLabel,
+        parentCategory: parentCat,
+      });
+    } else {
+      return res.json({ msg: "in createBrand conditionals" });
     }
-}
 
-const createBrand = async (req,res) => {
-    try{
-        let colLabel;
+    await newBrand.save();
 
-        const brandName = req.body.brandName;
-        const multiVar = req.body.multiVar;
-        const rowLabel = req.body.rowLabel
+    res.json({ msg: `${brandName} brand created!` });
+  } catch (err) {
+    console.error("Error creating brand:", err);
+    res.status(500).json({ message: "Internal server error!!(createBrand)" });
+  }
+};
 
-        if (req.body.colLabel) colLabel = req.body.colLabel;
+const delBrand = async (req, res) => {
+  try {
+    const delBrandId = req.body.delBrandId;
 
-        const parentCat = req.body.parentCat;
-
-        let newBrand;
-
-        if (multiVar === "yes"){
-            newBrand = new Brand({
-                brandName: brandName,
-                multiVar: multiVar,
-                rowLabel: rowLabel,
-                colLabel: colLabel,
-                parentCategory: parentCat
-            });
-        }
-        else if(multiVar==="no"){
-            newBrand = new Brand({
-                brandName: brandName,
-                multiVar: multiVar,
-                rowLabel: rowLabel,
-                parentCategory: parentCat
-            });
-        }else{
-            return res.json({msg: "in createBrand conditionals"})
-        }
-
-        await newBrand.save();
-
-        res.json({msg: `${brandName} brand created!`})
-
+    if (!delBrandId || delBrandId == null) {
+      console.log("No such brand found!");
+      return res.json({ err: "No such brand found!" });
     }
-    catch(err){
-        console.error("Error creating brand:", err);
-        res.status(500).json({ message: "Internal server error!!(createBrand)" });
+
+    const delBrand = await Brand.findById({ _id: delBrandId });
+    const delBrandName = delBrand.brandName;
+
+    await Brand.deleteOne({ _id: delBrandId });
+
+    return res.json({ msg: delBrandName + " brand deleted!" });
+  } catch (err) {
+    console.error("Error deleting category:", err);
+    res.status(500).json({ message: "Internal server error!!(delBrand)" });
+  }
+};
+
+const editBrand = async (req, res) => {
+  try {
+    const editBrandId = req.body.editBrandId;
+
+    const editBrandName = req.body.editBrandName;
+    const editBrandMultiVar = req.body.editBrandMultiVar;
+    const editBrandRowLabel = req.body.editBrandRowLabel;
+    const editBrandColLabel = req.body.editBrandColLabel;
+    const editBrandParentCat = req.body.editBrandParentCategory;
+
+    var toUpdateBrand = await Brand.findById({ _id: editBrandId });
+
+    if (!toUpdateBrand) {
+      return res.json({ err: "No such Brand found!" });
     }
-}
 
-const delBrand = async (req,res) =>{
-    try{
-        const delBrandId = req.body.delBrandId
+    //// only edit the edited
+    if (editBrandName) toUpdateBrand.brandName = editBrandName;
+    if (editBrandMultiVar) toUpdateBrand.multiVar = editBrandMultiVar;
+    if (editBrandRowLabel) toUpdateBrand.rowLabel = editBrandRowLabel;
+    if (editBrandColLabel) toUpdateBrand.colLabel = editBrandColLabel;
+    if (editBrandParentCat) toUpdateBrand.parentCategory = editBrandParentCat;
 
-        if (!delBrandId || delBrandId == null){
-            console.log("No such brand found!")
-            return res.json({ err: "No such brand found!"});
-        }
+    toUpdateBrand.save();
 
-        const delBrand = await Brand.findById({_id:delBrandId})
-        const delBrandName = delBrand.brandName;
+    return res.json({ msg: toUpdateBrand });
+  } catch (err) {
+    console.error("Error editing Brandegory:");
+    res.status(500).json({ message: "Internal server error!!(editBrand)" });
+  }
+};
 
-        await Brand.deleteOne({ _id: delBrandId});
-
-        return res.json({msg: delBrandName +" brand deleted!"});
-
-    }
-    catch(err){
-        console.error("Error deleting category:", err);
-        res.status(500).json({ message: "Internal server error!!(delBrand)" });
-    }
-}
-
-
-const editBrand = async(req,res) => {
-    try{
-        const editBrandId = req.body.editBrandId
-
-        const editBrandName = req.body.editBrandName
-        const editBrandMultiVar = req.body.editBrandMultiVar
-        const editBrandRowLabel = req.body.editBrandRowLabel
-        const editBrandColLabel = req.body.editBrandColLabel
-        const editBrandParentCat = req.body.editBrandParentCategory
-
-        var toUpdateBrand = await Brand.findById({ _id: editBrandId})
-
-        if(!toUpdateBrand){
-            return res.json({ err: "No such Brand found!"})
-        }
-
-        //// only edit the edited
-        if (editBrandName) toUpdateBrand.brandName = editBrandName;
-        if (editBrandMultiVar) toUpdateBrand.multiVar = editBrandMultiVar;
-        if (editBrandRowLabel) toUpdateBrand.rowLabel = editBrandRowLabel;
-        if (editBrandColLabel) toUpdateBrand.colLabel = editBrandColLabel;
-        if (editBrandParentCat) toUpdateBrand.parentCategory = editBrandParentCat;
-
-
-        toUpdateBrand.save()
-
-        return res.json({ msg : toUpdateBrand})
-    } 
-    catch(err){
-        console.error("Error editing Brandegory:");
-        res.status(500).json({ message: "Internal server error!!(editBrand)" });
-    }
-}
-
-
-
-module.exports = {getAllCategories, createCategory,delCategory,editCategory, getAllBrands, createBrand, delBrand, editBrand}
+module.exports = {
+  getAllCategories,
+  createCategory,
+  delCategory,
+  editCategory,
+  //   getAllBrands,
+  createBrand,
+  delBrand,
+  editBrand,
+  getSpecificBrand,
+};
--- Server/routers/data-router.js
+++ Server/routers/data-router.js
@@ -2,15 +2,23 @@ const express = require("express");
 
 const router = express.Router();
 
-const datac = require("../controllers/data-controller")
-
+const datac = require("../controllers/data-controller");
 
 router.get("/", (req, res) => {
-    res.send("Welcome to plywood IMSs api!");
+  res.send("Welcome to plywood IMSs api!");
 });
 
-router.route("/category").get(datac.getAllCategories).post(datac.createCategory).delete(datac.delCategory).patch(datac.editCategory)
-router.route("/brand").get(datac.getAllBrands).post(datac.createBrand).delete(datac.delBrand).patch(datac.editBrand)
-
+router
+  .route("/category")
+  .get(datac.getAllCategories)
+  .post(datac.createCategory)
+  .delete(datac.delCategory)
+  .patch(datac.editCategory);
+router.route("/brandList").post(datac.getSpecificBrand);
+router
+  .route("/brand")
+  .post(datac.createBrand)
+  .delete(datac.delBrand)
+  .patch(datac.editBrand);
 
 module.exports = router;
