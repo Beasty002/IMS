50b27b6bda3537ee3b9ed2158be0ee6cd75314bf
Sulav Gautam <118454666+SulAv100@users.noreply.github.com>
2024-10-06 19:05:00 +0545

    fix : quantum typo mistake

--- Client/src/pages/CatBrandList/CatBrandList.jsx
+++ Client/src/pages/CatBrandList/CatBrandList.jsx
@@ -96,7 +96,7 @@ export default function CatBrandList() {
         </div>
         <div className="btn-container">
           <button onClick={setBrandModel} className="primary-btn">
-            <i className="bx bx-plus-circle"></i> New Brand
+            <i className="bx bx-plus-circle"></i> New Type
           </button>
         </div>
       </div>
