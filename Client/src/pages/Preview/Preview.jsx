fe33dd416ee0a3aa3a8b58d9017ab26212b58a45
Sulav Gautam <118454666+SulAv100@users.noreply.github.com>
2024-11-08 13:39:30 +0545

    fix: asynchronous data recieve and state update

--- Client/src/pages/Preview/Preview.jsx
+++ Client/src/pages/Preview/Preview.jsx
@@ -7,235 +7,187 @@ function Preview() {
   const location = useLocation();
   const [isLoading, setIsLoading] = useState(false);
   const { selectedItems = [], selectedTitles = [] } = location.state || {};
-  const [matrixKey, setMatrixKey] = useState({});
-  const [brandId, setBrandId] = useState("");
-  const [title, setTitle] = useState("");
+  const [matrixKey, setMatrixKey] = useState([]);
   const [singleVarFetch, setSingleVarFetch] = useState([]);
 
-  useEffect(() => {
-    if (selectedItems.length > 0) {
-      selectedItems.forEach((item, index) => {
-        fetchTotalData(item, index);
-      });
-    }
-  }, [selectedItems]);
-
-  useEffect(() => {
-    console.log("YO ho hai fetch data", totalFetchData);
-  }, [totalFetchData]);
-
-  const fetchTotalData = async (brandId, index) => {
-    console.log("Fetching data for brandId:", brandId);
-    setBrandId(brandId);
-
+  const fetchDataForSelectedItems = async () => {
     setIsLoading(true);
     try {
-      const response = await fetch("http://localhost:3000/api/report", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ brandId }),
-      });
-      const data = await response.json();
-      if (!response.ok) {
-        console.log(response.statusText);
-        return;
-      }
-      setMatrixKey(Object.keys(data.matrix));
-      setTotalFetchData((prevState) => [
-        ...prevState,
-        { data, title: selectedTitles[index] || "Unnamed Report" },
-      ]);
-      setTitle(totalFetchData.title);
+      const allData = await Promise.all(
+        selectedItems.map(async (item, index) => {
+          const response = await fetch("http://localhost:3000/api/report", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ brandId: item }),
+          });
+          const data = await response.json();
+          if (!response.ok) throw new Error(response.statusText);
+
+          return { data, title: selectedTitles[index] || "Unnamed Report" };
+        })
+      );
+      setTotalFetchData(allData); 
+      setMatrixKey(allData.length ? Object.keys(allData[0].data.matrix) : []);
     } catch (error) {
-      console.error(error);
+      console.error("Error fetching data:", error);
     } finally {
       setIsLoading(false);
     }
   };
 
   useEffect(() => {
-    if (matrixKey && brandId) {
-      for (let i = 0; i <= matrixKey.length; i++) {
-        fetchSpecificSingleData(matrixKey[i], brandId);
-      }
-    }
-  }, [matrixKey]);
-
-  const fetchSpecificSingleData = async (matrixKey, brandId) => {
-    console.log(JSON.stringify({ matrixKey, brandId }));
-    try {
-      const response = await fetch("http://localhost:3000/api/report", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ matrixKey, brandId }),
-      });
-      const data = await response.json();
-
-      if (!response.ok) {
-        console.log(response.statusText);
-        return;
-      }
-      console.log(data);
-      setSingleVarFetch((prevState) => [...prevState, { data }]);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+    if (selectedItems.length > 0) fetchDataForSelectedItems();
+  }, [selectedItems]);
 
   useEffect(() => {
-    console.log("Yo ho hai te key", totalFetchData);
-  }, [totalFetchData]);
+    const fetchSingleData = async () => {
+      const fetchedData = await Promise.all(
+        matrixKey.map(async (key) => {
+          try {
+            const response = await fetch("http://localhost:3000/api/report", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({ matrixKey: key, brandId: selectedItems[0] }),
+            });
+            const data = await response.json();
+            if (!response.ok) throw new Error(response.statusText);
+            return { data, key };
+          } catch (error) {
+            console.error("Error fetching single data:", error);
+            return null;
+          }
+        })
+      );
+      setSingleVarFetch(fetchedData.filter(Boolean));
+    };
+
+    if (matrixKey.length && selectedItems.length) {
+      fetchSingleData();
+    }
+  }, [matrixKey, selectedItems]);
 
   return (
-    <>
-      <div className="total-out">
-        <div id="report-content">
-          {singleVarFetch.slice(0, -1).map((item, index) => {
-            return (
-              <div key={index} className="report-table-container">
-                <h1>{item.title}</h1>
+    <div className="total-out">
+      <div id="report-content">
+        {totalFetchData.map((item, index) => (
+          <div key={index} className="report-table-container">
+            <h1>{item.title}</h1>
+            {item.data && item.data.multiVar === true ? (
+              <table>
+                <thead className="report-table-head">
+                  <tr>
+                    <th rowSpan={2}>S.N</th>
+                    <th rowSpan={2}>
+                      {item.data.brandCol
+                        ? `${item.data.brandRow}/${item.data.brandCol}`
+                        : item.data.brandRow}
+                    </th>
+                    {item.data.allColumns?.map((col, colIndex) => (
+                      <th key={colIndex} colSpan={4}>
+                        {col.column}
+                      </th>
+                    ))}
+                  </tr>
+                  <tr>
+                    {item.data.allColumns?.map((_, colIndex) => (
+                      <React.Fragment key={colIndex}>
+                        <th>OP</th>
+                        <th>IN</th>
+                        <th>OUT</th>
+                        <th>BAL</th>
+                      </React.Fragment>
+                    ))}
+                  </tr>
+                </thead>
+                <tbody>
+                  {Object.entries(item.data.matrix || {}).map(
+                    ([rowLabel, rowData], rowIndex) => {
+                      if (!rowData || Object.keys(rowData).length === 0)
+                        return null;
 
-                {item.data && item.data.multiVar ? (
-                  <table>
+                      return (
+                        <tr key={rowIndex}>
+                          <td>{rowIndex + 1}</td>
+                          <td>{rowLabel}</td>
+                          {item.data.allColumns.map((col, colIndex) => {
+                            const colData = rowData[col.column];
+                            return (
+                              <React.Fragment key={colIndex}>
+                                <td>{colData?.op ?? ""}</td>
+                                <td>{colData?.in ?? ""}</td>
+                                <td>{colData?.out ?? ""}</td>
+                                <td>{colData?.bal ?? ""}</td>
+                              </React.Fragment>
+                            );
+                          })}
+                        </tr>
+                      );
+                    }
+                  )}
+                </tbody>
+              </table>
+            ) : (
+              item.data &&
+              Object.entries(item.data.matrix || {}).map(
+                ([key, value], innerIndex) => (
+                  <table
+                    key={innerIndex}
+                    style={{ marginBottom: "30px" }}
+                    className="table1"
+                  >
                     <thead className="report-table-head">
                       <tr>
-                        <th rowSpan={2}>S.N</th>
-                        <th rowSpan={2}>
-                          {item.data.brandCol
-                            ? `${item.data.brandRow}/${item.data.brandCol}`
-                            : item.data.brandRow}
-                        </th>
-                        {item.data.allColumns?.map((col, colIndex) => (
-                          <th key={colIndex} colSpan={4}>
-                            {col.column}
-                          </th>
-                        ))}
+                        <th rowSpan="2">S.N</th>
+                        <th rowSpan="2">Code</th>
+                        <th colSpan="4">{key}</th>
                       </tr>
                       <tr>
-                        {item.data.allColumns?.map((_, colIndex) => (
-                          <React.Fragment key={colIndex}>
-                            <th>OP</th>
-                            <th>IN</th>
-                            <th>OUT</th>
-                            <th>BAL</th>
-                          </React.Fragment>
-                        ))}
+                        <th>OP</th>
+                        <th>IN</th>
+                        <th>OUT</th>
+                        <th>BAL</th>
                       </tr>
                     </thead>
                     <tbody>
-                      {Object.entries(item.data.matrix || {}).map(
-                        ([rowLabel, rowData], rowIndex) => {
-                          if (!rowData || Object.keys(rowData).length === 0)
-                            return null;
-
-                          return (
-                            <tr key={rowIndex}>
-                              <td>{rowIndex + 1}</td>
-                              <td>{rowLabel}</td>
-                              {item.data.allColumns.map((col, colIndex) => {
-                                const colData = rowData[col.column];
-
-                                return (
-                                  <React.Fragment key={colIndex}>
-                                    <td>
-                                      {colData?.op !== undefined
-                                        ? colData.op
-                                        : ""}
-                                    </td>
-                                    <td>
-                                      {colData?.in !== undefined
-                                        ? colData.in
-                                        : ""}
-                                    </td>
-                                    <td>
-                                      {colData?.out !== undefined
-                                        ? colData.out
-                                        : ""}
-                                    </td>
-                                    <td>
-                                      {colData?.bal !== undefined
-                                        ? colData.bal
-                                        : ""}
-                                    </td>
-                                  </React.Fragment>
-                                );
-                              })}
-                            </tr>
-                          );
-                        }
+                      {Object.entries(value || {}).map(
+                        ([innerKey, innerValue], innerIndex) => (
+                          <tr key={innerIndex}>
+                            <td>{innerIndex + 1}</td>
+                            <td>{innerKey}</td>
+                            {Object.entries(innerValue || {}).map(
+                              ([_, val], colIndex) => (
+                                <td key={colIndex}>{val}</td>
+                              )
+                            )}
+                          </tr>
+                        )
                       )}
                     </tbody>
                   </table>
-                ) : (
-                  item.data &&
-                  Object.entries(item.data.matrix || {}).map(
-                    ([key, value], index) => (
-                      <table
-                        key={index}
-                        style={{ marginBottom: "30px" }}
-                        className="table1"
-                      >
-                        <thead className="report-table-head">
-                          <tr>
-                            <th rowSpan="2">S.N</th>
-                            <th rowSpan="2">Code</th>
-                            <th colSpan="4">{key}</th>
-                          </tr>
-                          <tr>
-                            <th>OP</th>
-                            <th>IN</th>
-                            <th>OUT</th>
-                            <th>BAL</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          {Object.entries(value || {}).map(
-                            ([innerKey, innerValue], innerIndex) => (
-                              <tr key={innerIndex}>
-                                <td>{innerIndex + 1}</td>
-                                <td>{innerKey}</td>
-                                {Object.entries(innerValue || {}).map(
-                                  ([_, value], colIndex) => (
-                                    <td key={colIndex}>{value}</td>
-                                  )
-                                )}
-                              </tr>
-                            )
-                          )}
-                        </tbody>
-                      </table>
-                    )
-                  )
-                )}
-              </div>
-            );
-          })}
-        </div>
-        <button
-          onClick={() => window.print()}
-          className="primary-btn"
-          id="print-btn"
-        >
-          Print Report
-        </button>
+                )
+              )
+            )}
+          </div>
+        ))}
       </div>
+      <button onClick={() => window.print()} className="primary-btn" id="print-btn">
+        Print Report
+      </button>
       {isLoading && (
         <div className="center-hanne">
           <div className="bhitri-center">
-            <span
-              style={{ textAlign: "center", color: "white", fontSize: "25px" }}
-            >
+            <span style={{ textAlign: "center", color: "white", fontSize: "25px" }}>
               Generating Report
             </span>
             <div className="box"></div>
           </div>
         </div>
       )}
-    </>
+    </div>
   );
 }
 
