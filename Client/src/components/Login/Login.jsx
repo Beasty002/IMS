a8675df997daa51d20d69d13bb23fc6139dd2226
Sulav Gautam <118454666+SulAv100@users.noreply.github.com>
2024-11-09 20:12:51 +0545

    fix: fetch link

--- Client/src/components/Login/Login.jsx
+++ Client/src/components/Login/Login.jsx
@@ -23,7 +23,7 @@ function Login() {
       return;
     }
     try {
-      const response = await fetch("http://localhost:3000/api/login", {
+      const response = await fetch("http://localhost:3000/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
